a8d4c4de8e13c8691cf1b7ab1e09b45436ef9247
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-05-18 20:14:42 +0500

    Updated UI

--- useend/urls.py
+++ useend/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/',RegisterPage.as_view(),name='register'),
-    path('',TaskList.as_view(),name='tasks'),
-    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
-    path('task-create/',TaskCreate.as_view(),name='task-create'),
-    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
-    path('task-delete/<int:pk>/',TaskDelete.as_view(),name='task-delete')
+   
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
