51c2367f45f4a02483005e8f7241c6c553be4cbd
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-05-19 00:23:52 +0500

    Applying Reordering

--- useend/models.py
+++ useend/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+ 
 class Task(models.Model):
     
     PRIORITY_LIST = (
@@ -12,7 +12,7 @@ class Task(models.Model):
 
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     title = models.CharField(max_length=250)
-    priority = models.CharField(choices=PRIORITY_LIST, max_length=128)
+    priority = models.CharField(max_length=20)
     description = models.CharField(max_length=250,null=False,blank=False)
     complete = models.BooleanField(default=False)
     when_created = models.DateTimeField(auto_now_add=True)
