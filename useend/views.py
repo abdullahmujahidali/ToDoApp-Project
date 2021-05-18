e5ae283f26ec2c26094cefceb519885bb6549eb2
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-05-18 14:16:30 +0500

    Register route created and all other routes are updated

--- useend/views.py
+++ useend/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render,redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView,FormView
@@ -24,15 +24,27 @@ class RegisterPage(FormView):
     template_name='useend/register.html'
     form_class = UserCreationForm
     redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self,form):
+        user = form.save()
+        if user is not None:
+            login(self.request,user)
+        return super(RegisterPage,self).form_valid(form)
+
+    def get(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage,self).get(*args,**kwargs)
 
 class TaskList(LoginRequiredMixin,ListView):
     model = Task
-    context_object_name = 'todoTasks'
+    context_object_name = 'tasks'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context[' todoTasks'] = context['todoTasks'].filter(user=self.request.user)
-        context['count'] = context['todoTasks'].filter(complete=False).count()
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
         return context
 
 
